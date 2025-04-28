56aec519baa3b96907b7c80eaaed2aa49321f94c
es-house <e.sapio64@gmail.com>
2025-04-28 11:14:46 +0200

    Implemented inversion of gravity. Added player rotation when the gravity is inverted.

---
+++ Assets/Scripts/Managers/GravityManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class GravityManager : MonoBehaviour
+{
+
+    public bool isGravityReversed = false;
+    public static GravityManager Instance;
+
+    void Awake() {
+        if (Instance != null && Instance != this) {
+            Destroy(this);
+        } else {
+            Instance = this;
+        }
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void InvertGravity() {
+        if (isGravityReversed) {
+            Physics.gravity = new Vector3(0f, -9.81f, 0f);
+        } else {
+            Physics.gravity = new Vector3(0f, 9.81f, 0f);
+        }
+        isGravityReversed = !isGravityReversed;
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     private float playerSpeed = 10;
     private Rigidbody playerRigidbody;
     private Vector3 playerInput;
+    private Quaternion targetRotation;
+    private float rotationSpeed = 200f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
@@ -20,6 +22,10 @@ public class PlayerController : MonoBehaviour
         }
 
         playerInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+
+        if (Input.GetKeyDown(KeyCode.G)) {
+            FlipGravity();
+        }
         
     }
 
@@ -28,7 +34,27 @@ public class PlayerController : MonoBehaviour
             return;
         }
 
+        MovePlayer();
+
+        FlipPlayer();
+
+    }
+
+    private void FlipGravity() {
+        GravityManager.Instance.InvertGravity();
+        if (GravityManager.Instance.isGravityReversed) {
+            targetRotation = Quaternion.Euler(0f, 0f, transform.eulerAngles.z + 180f);
+        } else {
+            targetRotation = Quaternion.Euler(0f, 0f, transform.eulerAngles.z + 180f);
+        }
+    }
+
+    private void MovePlayer() {
         Vector3 movement = playerInput * playerSpeed;
         playerRigidbody.AddForce(movement);
+    }
+
+    private void FlipPlayer() {
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 }
