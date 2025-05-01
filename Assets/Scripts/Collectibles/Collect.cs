1db75b9d81060d87dcf1fb004a48cd9cb4f21875
es-house <e.sapio64@gmail.com>
2025-05-01 16:00:52 +0200

    Added audio to key and goal

--- Assets/Scripts/Collectibles/Collect.cs
+++ Assets/Scripts/Collectibles/Collect.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 [RequireComponent(typeof(Collider))]
 public class Collect : MonoBehaviour
 {
+    [SerializeField]
+    private AudioClip audioClip;
 
     void OnTriggerEnter(Collider other) {
         if (other.CompareTag(Utility.PLAYER_TAG)) {
@@ -11,8 +13,22 @@ public class Collect : MonoBehaviour
             } else if (gameObject.CompareTag(Utility.GOAL_TAG)) {
                 PlayerPrefs.SetInt(Utility.Collectibles.GOAL.ToString(), 1);
             }
+            if (audioClip != null) {
+                Play2DAudio(audioClip);
+            }
             PlayerPrefs.Save();
             Destroy(gameObject, .2f);
         }
+    }
+
+    private void Play2DAudio(AudioClip audioClipToPlay) {
+        GameObject audio = new GameObject("Audio");
+        AudioSource audioSource = audio.AddComponent<AudioSource>();
+        audioSource.clip = audioClipToPlay;
+        audioSource.spatialBlend = 0f;
+        audioSource.volume = .6f;
+        audioSource.Play();
+
+        Destroy(audio, audioClipToPlay.length);
     }
 }
